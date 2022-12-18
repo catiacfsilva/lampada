d8444f22c1ce7ac1dcdb8b6a385d50e09c724b39
catiacfsilva <115549806+catiacfsilva@users.noreply.github.com>
2022-12-18 16:43:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function changeImage() {
+    if (
+      document.getElementById("minhaimagem").src ="img/Light-Bulb-PNG-Photo.png"
+    ) {
+      document.getElementById("minhaimagem").src ="img/Light-Bulb-PNG-Photo.png";
+      document.getElementById("switch").innerHTML = "Desligar";
+      document.getElementById("main").classList.remove("lightoff");
+      document.getElementById("main").classList.add("lighton");
+    } else if (
+      document.getElementById("minhaimagem").src ="img/Light-Bulb-PNG-Photo.png"
+    ) {
+      document.getElementById("minhaimagem").src ="img/Light-Bulb-PNG-Photo.png";
+      document.getElementById("switch").innerHTML = "Ligar";
+      document.getElementById("main").classList.add("lightoff");
+      document.getElementById("main").classList.remove("lighton");
+    }
+  }
+  
+  document.getElementById("switch").addEventListener("click", changeImage);
+
+  
+  
